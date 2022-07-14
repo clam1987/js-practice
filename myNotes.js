26c8bd0d508703949e695c94cc3fb54a21568c2a
Wilson Lam <48064335+clam1987@users.noreply.github.com>
2022-07-13 18:28:29 -0700

    comments and notes

--- myNotes.js
+++ myNotes.js
@@ -1,21 +1,27 @@
+// use a document ready statement here
+// window.addEventListener('DOMContentLoaded', (event) => {
+//   console.log('DOM fully loaded and parsed');
+// });
 
 let addBtn = document.getElementById("add-btn"); 
 let addTxt = document.getElementById("note-text"); 
 let addTitle = document.getElementById("note-title"); 
-
 
 addBtn.addEventListener("click", function(e) {
 
     if(addTitle.value == "" || addTxt.value == "") {
         return alert("Please add note title and details"); 
     }
-    
+    // could technically build this into one statement
+    // I.E. let notes = JSON.parse(localStorage.getItem("notes")) || [];
     let notes = localStorage.getItem("notes"); 
     if (notes == null) {
+      // this technically works in javascript only due to how javascripts instantiates variables. If going with this logic, you should instantiate in the global scope.
         notesobj = []; 
     } else {
         notesObj = JSON.parse(notes); 
     }
+    // variables should represent what you are defining, in this case obj could be notes.
     let myObj = {
         title: addTitle.value, 
         text: addTxt.value 
@@ -27,7 +33,9 @@ addBtn.addEventListener("click", function(e) {
 
 // Function to show elements from localStorage
 function showNotes() {
+  // same as above, could technically build this into one statement.
     let notes = localStorage.getItem("notes");
+    // if instaniated in the global scope, you wouldn't need to declare notesObj twice.
     if (notes == null) {
       notesObj = [];
     } else {
@@ -46,6 +54,7 @@ function showNotes() {
     });
     let notesElm = document.getElementById("notes");
     if (notesObj.length != 0) {
+      // NICE used innerhtml correctly!
       notesElm.innerHTML = html;
     } else {
       notesElm.innerHTML = `No Notes Yet! Add a note using the form above.`;
@@ -55,14 +64,20 @@ function showNotes() {
 function deleteNote(index) {
     let confirmDel = confirm("Delete this note?");
     if (confirmDel == true) {
+      // same comments as above.
         let notes = localStorage.getItem("notes");
         if (notes == null) {
             notesObj = [];
         } else {
         notesObj = JSON.parse(notes);
         }
-    
+        // could use an array method filter to set this.
         notesObj.splice(index, 1);
+        // notesObj.filter(function(note, i) {
+        //   if(index === i) {
+        //     return note
+        //   }
+        // })
         localStorage.setItem("notes", JSON.stringify(notesObj));
         showNotes();
         }
